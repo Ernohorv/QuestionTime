782186aae1fc5cd81677339f83746431ac198921
Tamás <1234567zzz@gmail.com>
2018-02-21 22:45:48 +0100

    Added Firebase
    Added the following Firebase features:
     - Firestore
     - Authentication
     - Messaging

--- App.js
+++ App.js
@@ -6,53 +6,47 @@
 
 import React, { Component } from 'react';
 import {
-  Platform,
-  StyleSheet,
   Text,
   View
 } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import firebase from 'react-native-firebase';
 
-type Props = {};
-export default class App extends Component<Props> {
+
+export default class App extends Component {
+
+  componentWillMount() {
+    firebase.auth()
+      .signInAndRetrieveDataWithEmailAndPassword('1234567zzz@gmail.com', '123456')
+      .then(credential => {
+        if (credential) {
+          firebase.messaging().getInitialNotification().then(notif => {
+            //console.warn("INITIAL NOTIFICATION", notif)
+          });
+          firebase.messaging().onMessage(payload => {
+            /*firebase.messaging().createLocalNotification({
+              title: 'My app',
+              body: 'Hello world',
+              icon: 'ic_launcher',
+              priority: 'high',
+              click_action: 'ACTION',
+              show_in_foreground: true,
+              sound: 'default',
+              opened_from_tray: true
+            })*/
+          });
+        }
+      }).catch((error) => {
+        console.warn("Api call error");
+        alert(error.message);
+      });
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+      <View>
+        <Text>AUT Semester Project</Text>
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
